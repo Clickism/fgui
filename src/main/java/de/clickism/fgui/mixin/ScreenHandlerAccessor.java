9694977a9188b1d05f58baef8b3e7477158658f9
Clickism <yagizaktasx@gmail.com>
2026-01-11 16:15:02 +0100

    fix mixins

--- src/main/java/de/clickism/fgui/mixin/ScreenHandlerAccessor.java
+++ src/main/java/de/clickism/fgui/mixin/ScreenHandlerAccessor.java
@@ -1,13 +1,16 @@
 package de.clickism.fgui.mixin;
-//? if >=1.21.5 {
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.RemoteSlot;
 import org.spongepowered.asm.mixin.Mixin;
+
+//? if >=1.21.5 {
+import net.minecraft.world.inventory.RemoteSlot;
 import org.spongepowered.asm.mixin.gen.Accessor;
+//?}
 
 @Mixin(AbstractContainerMenu.class)
 public interface ScreenHandlerAccessor {
+    //? if >=1.21.5 {
     @Accessor
     RemoteSlot getRemoteCarried();
+    //?}
 }
-//?}
--- src/main/java/de/clickism/fgui/mixin/StaticAccessor.java
+++ src/main/java/de/clickism/fgui/mixin/StaticAccessor.java
@@ -1,17 +1,24 @@
 package de.clickism.fgui.mixin;
-//? if >=1.21.10 {
-import com.mojang.authlib.GameProfile;
-import com.mojang.datafixers.util.Either;
-import net.minecraft.world.entity.player.PlayerSkin;
 import net.minecraft.world.item.component.ResolvableProfile;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
+//? if >=1.21.10 {
+import net.minecraft.world.entity.player.PlayerSkin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import com.mojang.authlib.GameProfile;
+import com.mojang.datafixers.util.Either;
+//?}
+
+//? if >=1.21.10 {
 @Mixin(ResolvableProfile.Static.class)
+//?} else {
+/*@Mixin(ResolvableProfile.class)
+*///?}
 public interface StaticAccessor {
+    //? if >=1.21.10 {
     @Invoker("<init>")
     static ResolvableProfile.Static createStatic(Either<GameProfile, ResolvableProfile.Partial> profileOrData, PlayerSkin.Patch mannequinInfo) {
         throw new UnsupportedOperationException();
     }
+    //?}
 }
-//?}
